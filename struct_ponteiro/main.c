81094d3f0a1acc0299ec16a41e2a52d5403ccb8b
Lucassoli <lucassupri@gmail.com>
2024-04-09 10:32:03 -0300

    att

--- struct_ponteiro/main.c
+++ struct_ponteiro/main.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char* argv[]){
+    typedef struct Ponto{
+        int x;
+        int y;
+    } Ponto;
 
-    int a = 2;
-    int b = a;
-    int *p = &a;
+    int main (int argc, char*argv[]) {
 
-    printf("a = %d \t  b = %d\n", a, b);
-    printf("&a = %p \t &b = %p\n", &a, &b);
+        int a = 2;
+        int b = 2;
+        int *p = &a;
 
-    b = 3;
-    printf("a = %d \t  b = %d\n", a, b);
+        printf("a= %d \t b= %d\n ", a, b);
+        printf("a= %d \t b= %d\n", &a, &b);
 
-    a = 4;
-    printf("a = %d \t  b = %d\n", a, b);
+        b = 3;
+        printf("a= %d\t b= %d \n", a, b);
 
-    printf("&p = %p \t  p = %p \t *p = %d\n", &p, p, *p);
+        a = 4;
+        printf("a = %d \t b = %d \n", a, b);
 
-    *p = 5;
-    printf("a = %d \t  b = %d\n", a, b);
+        printf("&p= %p \t p= %p \t *p= %d \n", &p, p, *p);
 
-    exit(0);
+        *p =5;
+        printf("a = %d \t b= %d \n", a, b);
 
-}
+        p = malloc(sizeof(int)*5);
+        *p = 3;
+        p[0] = 3;
+        p[1] = 55;
+
+        printf("&p = %p \t p = %p \t *p = %d\n", &p, p, *p);
+        printf("&p = %p \t p = %p \t *p = %d\n", &p, &p[1], p[1]);
+
+        Ponto A;
+        A.x = 4; //acessar um inteiro dentro de A
+        A.y = 4;
+        printf("A(%d, %d) \n", A.x, A.y);
+
+        //printf("",);
+        //printf("a = %d \t b = %d\n", a,b); 
+
+        exit(0);
+    }
